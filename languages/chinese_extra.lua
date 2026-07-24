eb566ce52c4700a6918315dc3a603ce5567caa8e
随波逐流 <2247564152@qq.com>
2026-07-24 22:51:59 +0800

    update

--- languages/chinese_extra.lua
+++ languages/chinese_extra.lua
@@ -164,6 +164,7 @@ chinese.stealth = "隐身"
 chinese.translucent = "半透明"
 chinese.visible = "现形"
 chinese.needsPower = "需要电力"
+chinese.failureToPerfect = "无法达成完美回合"
 --律法左手玩法相关
 --律法左手对讲机
 chinese.not_enough_evidences = "目前证据不足，还需要搜集"
